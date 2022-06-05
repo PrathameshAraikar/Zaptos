d001b4b76b946150ade66930970a22fe0405598c
Prathamesh Araikar <prathamesharaikar11@gmail.com>
2022-06-05 16:05:13 +0530

    Initial Commit

---
+++ Zaptos/ZaptosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZaptosApp.swift
+//  Zaptos
+//
+//  Created by Prathamesh Araikar on 05/06/22.
+//
+
+import SwiftUI
+
+@main
+struct ZaptosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
